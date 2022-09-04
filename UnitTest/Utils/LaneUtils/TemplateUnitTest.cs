3538ce38a070f515f2a0536238bfd7f95a978d2b
jamesworden <jamesworden.ny@gmail.com>
2022-09-03 22:02:53 -0400

    Added template unit test

---
+++ UnitTest/Utils/LaneUtils/TemplateUnitTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace UnitTest
+{
+    public class MethodName
+    {
+        [Fact]
+        public void Should_Do_A_Thing()
+        {
+            Assert.True(true);
+        }
+    }
+}
