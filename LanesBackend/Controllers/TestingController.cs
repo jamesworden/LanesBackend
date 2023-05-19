140d20588f0dc993156af280a16c004b9fdb9b93
jamesworden <jamesworden.ny@gmail.com>
2023-05-19 17:51:53 -0400

    Use swagger to detect available api endpoints

---
+++ LanesBackend/Controllers/TestingController.cs
@@ -0,0 +1,16 @@
+﻿using LanesBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanesBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestingController : ControllerBase
+    {
+        [HttpPost(Name = "UpdateGameWithTestData")]
+        public string UpdateGameWithTestData(string gameCode, [FromBody] TestingGameData testingGameData)
+        {
+            return "Test";
+        }
+    }
+}
---
+++ LanesBackend/Models/TestingGameData.cs
@@ -0,0 +1,7 @@
+﻿namespace LanesBackend.Models
+{
+    public class TestingGameData
+    {
+        public Lane[] Lanes = new Lane[5];
+    }
+}
