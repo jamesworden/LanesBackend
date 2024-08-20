90f3d56657d26a54926ef8a31b313850ae3baa37
jamesworden <jamesworden.ny@gmail.com>
2024-08-20 17:57:40 -0400

    Another fix

--- src/ClassroomGroups/ClassroomGroups.Api/Features/Classrooms/ClassroomsController.cs
+++ src/ClassroomGroups/ClassroomGroups.Api/Features/Classrooms/ClassroomsController.cs
@@ -1,5 +1,5 @@
 using ClassroomGroups.Application.Features.Classrooms.Requests;
-using ClassroomGroups.Domain.Features.Classrooms;
+using ClassroomGroups.Domain.Features.Classrooms.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
