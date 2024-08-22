e54c3160e4fdccecaf742cbe85b6a47fc47a2bc2
jamesworden <jamesworden.ny@gmail.com>
2024-08-22 16:16:40 -0400

    Try to remove comments

--- deploy-scripts/before_install.sh
+++ deploy-scripts/before_install.sh
@@ -1,11 +1,8 @@
-﻿# Remove installed code and the systemd service file
-rm -rf /var/www/*
+﻿rm -rf /var/www/*
 rm -rf /etc/systemd/system/webapi.service
 
-# Fetch the environment variables from SSM Parameter Store
 clientId=$(aws ssm get-parameter --name "/ClassroomGroups/Google/ClientId" --region "us-east-1" --query "Parameter.Value" --output text)
 clientSecret=$(aws ssm get-parameter --name "/ClassroomGroups/Google/ClientSecret" --region "us-east-1" --query "Parameter.Value" --output text)
 
-# Set the environment variables
 export ClassroomGroups__Authentication__Google__ClientId=$clientId
 export ClassroomGroups__Authentication__Google__ClientSecret=$clientSecret
