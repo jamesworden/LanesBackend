0169f21ba9348feeae41ff1333e9229e0eb5d099
jamesworden <jamesworden.ny@gmail.com>
2024-08-22 16:24:01 -0400

    Add log

--- deploy-scripts/start_app.sh
+++ deploy-scripts/start_app.sh
@@ -1,3 +1,3 @@
-﻿# Remove installed code and the systemd service file
+﻿echo "Remove installed code and start the systemd service file..."
 systemctl stop webapi.service
 systemctl start webapi.service
