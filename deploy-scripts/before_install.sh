b97d0abbf66f5a25991c09f15a696962f85ec6ce
jamesworden <jamesworden.ny@gmail.com>
2024-08-22 17:03:29 -0400

    test

--- deploy-scripts/before_install.sh
+++ deploy-scripts/before_install.sh
@@ -8,6 +8,9 @@ echo "[Before Installing App] Fetching app secrets from aws parameter store..."
 clientId=$(aws ssm get-parameter --name "/ClassroomGroups/Google/ClientId" --region "us-east-1" --query "Parameter.Value" --output text)
 clientSecret=$(aws ssm get-parameter --name "/ClassroomGroups/Google/ClientSecret" --region "us-east-1" --query "Parameter.Value" --output text)
 
+echo "[Before Installing App] /ClassroomGroups/Google/ClientId: $clientId"
+echo "[Before Installing App] /ClassroomGroups/Google/ClientSecret: $clientSecret"
+
 echo "[Before Installing App] Persisting app secrets as environment variables..."
 export ClassroomGroups__Authentication__Google__ClientId=$clientId
 export ClassroomGroups__Authentication__Google__ClientSecret=$clientSecret
