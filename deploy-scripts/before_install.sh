6f64bf811ca4e84eca6b1e13850a071e8e43eb2c
jamesworden <jamesworden.ny@gmail.com>
2024-08-22 16:10:31 -0400

    try new method

--- deploy-scripts/before_install.sh
+++ deploy-scripts/before_install.sh
@@ -1,5 +1,15 @@
-﻿#!/bin/bash
-
-# Remove installed code and the systemd service file
+﻿# Remove installed code and the systemd service file
 rm -rf /var/www/*
 rm -rf /etc/systemd/system/webapi.service
+
+# Install AWS CLI if not already installed
+sudo apt-get update
+sudo apt-get install -y awscli
+
+# Fetch the environment variables from SSM Parameter Store
+clientId=$(aws ssm get-parameter --name "/ClassroomGroups/Google/ClientId" --query "Parameter.Value" --output text)
+clientSecret=$(aws ssm get-parameter --name "/ClassroomGroups/Google/ClientSecret" --query "Parameter.Value" --output text)
+
+# Set the environment variables
+export ClassroomGroups__Authentication__Google__ClientId=$clientId
+export ClassroomGroups__Authentication__Google__ClientSecret=$clientSecret
