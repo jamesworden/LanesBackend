89aa0644dad96db0235d5794c4312fd76cc6e413
jamesworden <jamesworden.ny@gmail.com>
2024-09-12 09:24:18 -0400

    Thing

--- src/ClassroomGroups/ClassroomGroups.Application/Features/Classrooms/DeleteClassroom.cs
+++ src/ClassroomGroups/ClassroomGroups.Application/Features/Classrooms/DeleteClassroom.cs
@@ -7,31 +7,28 @@ namespace ClassroomGroups.Application.Features.Classrooms;
 
 public record DeleteClassroomResponse(Classroom DeletedClassroom) { }
 
-public record DeleteClassroomRequest(Guid ClassroomId) : IRequest<DeleteClassroomResponse?> { }
+public record DeleteClassroomRequest(Guid ClassroomId) : IRequest<DeleteClassroomResponse> { }
 
 public class DeleteClassroomRequestHandler(
   ClassroomGroupsContext dbContext,
   AuthBehaviorCache authBehaviorCache
-) : IRequestHandler<DeleteClassroomRequest, DeleteClassroomResponse?>
+) : IRequestHandler<DeleteClassroomRequest, DeleteClassroomResponse>
 {
   readonly ClassroomGroupsContext _dbContext = dbContext;
 
   readonly AuthBehaviorCache authBehaviorCache = authBehaviorCache;
 
-  public async Task<DeleteClassroomResponse?> Handle(
+  public async Task<DeleteClassroomResponse> Handle(
     DeleteClassroomRequest request,
     CancellationToken cancellationToken
   )
   {
     var account = authBehaviorCache.Account ?? throw new Exception();
 
-    var classroom = _dbContext.Classrooms.SingleOrDefault(c =>
-      c.Id == request.ClassroomId && c.AccountId == account.Id
-    );
-    if (classroom is null)
-    {
-      return null;
-    }
+    var classroom =
+      _dbContext.Classrooms.SingleOrDefault(c =>
+        c.Id == request.ClassroomId && c.AccountId == account.Id
+      ) ?? throw new Exception();
 
     _dbContext.Classrooms.Remove(classroom);
     await _dbContext.SaveChangesAsync(cancellationToken);
