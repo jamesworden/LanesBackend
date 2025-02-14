cab445c3842c3cd76b1ddcbfe14f77697368b06f
James Worden <james@jamesworden.com>
2025-02-14 15:36:43 -0500

    Use industry standard library for authenticating with google

--- src/LanesBackendLauncher/Program.cs
+++ src/LanesBackendLauncher/Program.cs
@@ -13,10 +13,10 @@ using ClassroomGroups.Application.Features.Authentication;
 using ClassroomGroups.Application.Features.Classrooms;
 using ClassroomGroups.Application.Features.Classrooms.Shared;
 using ClassroomGroups.DataAccess.Contexts;
+using Google.Apis.Auth.AspNetCore3;
 using LanesBackendLauncher.Util;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
@@ -46,6 +46,8 @@ builder.Services.AddHostedService<DatabaseBackupService>();
 builder
   .Services.AddAuthentication(options =>
   {
+    options.DefaultChallengeScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+    options.DefaultForbidScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
   })
   .AddCookie(options =>
@@ -61,16 +63,23 @@ builder
       return Task.CompletedTask;
     };
   })
-  .AddGoogle(
-    GoogleDefaults.AuthenticationScheme,
-    options =>
+  .AddGoogleOpenIdConnect(options =>
+  {
+    options.ClientId =
+      builder.Configuration["ClassroomGroups:Authentication:Google:ClientId"] ?? "";
+    options.ClientSecret =
+      builder.Configuration["ClassroomGroups:Authentication:Google:ClientSecret"] ?? "";
+    options.CallbackPath = "/login-with-google-response";
+
+    options.SignedOutRedirectUri =
+      builder.Configuration["ClassroomGroups:LoggedOutRedirectUrl"] ?? "";
+
+    options.Events.OnTicketReceived = async context =>
     {
-      options.ClientId =
-        builder.Configuration["ClassroomGroups:Authentication:Google:ClientId"] ?? "";
-      options.ClientSecret =
-        builder.Configuration["ClassroomGroups:Authentication:Google:ClientSecret"] ?? "";
-    }
-  );
+      context.ReturnUri = builder.Configuration["ClassroomGroups:LoggedInRedirectUrl"] ?? "";
+      await Task.CompletedTask;
+    };
+  });
 
 builder.Services.AddAuthorization();
 
@@ -100,7 +109,6 @@ builder
   {
     options.PayloadSerializerOptions.Converters.Add(new JsonStringEnumConverter());
   });
-builder.Services.AddCors();
 
 // [ChessOfCards Service Registry]
 builder.Services.AddSingleton<IPendingGameRepository, PendingGameRepository>();
